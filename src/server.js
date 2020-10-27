5abfa29ec8fa477638733f948bff88c00ccabbc4
iknowahra <iknowahrajoh@gmail.com>
2020-10-27 22:34:36 +0700

    fix line rules

--- src/server.js
+++ src/server.js
@@ -3,25 +3,25 @@ dotenv.config();
 import { GraphQLServer } from 'graphql-yoga';
 import logger from 'morgan';
 import schema from './schema';
-import  { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({schema, context: {prisma,}});
+const server = new GraphQLServer({ schema, context: { prisma } });
 
 const options = {
   port: PORT,
   endpoint: '/graphql',
   subscriptions: '/subscriptions',
   playground: '/playground',
-}
+};
 
-server.express.use(logger("dev"));
+server.express.use(logger('dev'));
 
 server.start(options, ({ port }) =>
   console.log(
     `Server started, listening on port ${port} for incoming requests.`,
   ),
-)
+);
