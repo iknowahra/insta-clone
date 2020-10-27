9e491dbf62c1259e37c59acd7fb2ffada8ba160e
iknowahra <iknowahrajoh@gmail.com>
2020-10-27 22:25:40 +0700

    create server and graphql schema

---
+++ src/schema.js
@@ -0,0 +1,14 @@
+import path from "path";
+import { makeExecutableSchema } from "apollo-server";
+import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+import { loadFilesSync } from '@graphql-tools/load-files';
+
+const allTypes = loadFilesSync(path.join(__dirname, "/api/**/*.graphql"));
+const allResolvers = loadFilesSync(path.join(__dirname, "/api/**/*.resolver.js"));
+
+const schema = makeExecutableSchema({
+typeDefs: mergeTypeDefs(allTypes),
+resolvers: mergeResolvers(allResolvers)
+});
+
+export default schema;
