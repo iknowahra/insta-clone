b6c9a171f4c2a209bc0328026b1f5664b49687d4
iknowahra <iknowahrajoh@gmail.com>
2020-10-29 20:51:39 +0700

    request Secret email api

--- src/api/User/requestSecret/requestSecret.resolver.js
+++ src/api/User/requestSecret/requestSecret.resolver.js
@@ -1,18 +1,19 @@
-import { secretGenerator } from '../../../utils/utils';
+import { secretGenerator, sendSecretMail } from '../../../utils/utils';
 
 export default {
   Mutation: {
     requestSecret: async (_, args, context) => {
-      const { email } = args;
+      const { email, userName } = args;
       const loginSecret = secretGenerator();
       try {
+        await sendSecretMail(email, userName, loginSecret);
         await context.prisma.user.update({
           where: { email },
           data: { loginSecret },
         });
         return true;
       } catch (error) {
-        return false;
+        return error;
       }
     },
   },
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,27 @@
+import * as dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 import { adjectives, nouns } from './words';
+import mailgun from 'mailgun-js';
 
 export const secretGenerator = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
 
-export const sendMail = email => null;
+export const sendSecretMail = (address, userName, secret) => {
+  const domain = process.env.MAILGUN_DOMAIN;
+  const apiKey = process.env.MAILGUN_API;
+  const mg = mailgun({ apiKey, domain });
+  const mailOptions = {
+    from: `InstaClone <instaClone@${domain}>`,
+    to: address,
+    subject: '🔒 InstaClone Enter Key 🔒',
+    html: `Dear Ms/Mr.${userName},</br>Welcome to join InstaClone 🎉</br>Your secret key is <strong>${secret}</strong>.</br>Copy paste on our app/website to log in.`,
+  };
 
-export const sendSecretMail = (address, secret) => {};
+  return mg.messages().send(mailOptions, function (error, body) {
+    console.log(body);
+  });
+};
