07117c459c9b1d819c6b4bec4d15d7b1568bf2d4
iknowahra <iknowahrajoh@gmail.com>
2020-10-31 21:07:23 +0700

    add passport-jwt

---
+++ src/utils/passport.js
@@ -0,0 +1,37 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import passport from 'passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+const verifyUser = async (payload, done) => {
+  try {
+    const user = await prisma.user.findOne({ where: { id: payload.id } });
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (error) {
+    return done(error, false);
+  }
+};
+
+export const authenticateJwt = (req, res, next) =>
+  passport.authenticate('jwt', { sessions: false }, (error, user) => {
+    if (user) {
+      req.user = user;
+    }
+    next();
+  })(req, res, next);
+
+passport.use(new Strategy(jwtOptions, verifyUser));
+passport.initialize();
