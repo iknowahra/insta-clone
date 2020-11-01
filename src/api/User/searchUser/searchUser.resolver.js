1e10160884dce111cf797cc3d16960b777f99941
iknowahra <iknowahrajoh@gmail.com>
2020-11-01 14:44:42 +0700

    search User api

---
+++ src/api/User/searchUser/searchUser.resolver.js
@@ -0,0 +1,28 @@
+export default {
+  Query: {
+    searchUser: async (_, args, { prisma }) => {
+      const { term } = args;
+      return await prisma.user.findMany({
+        where: {
+          OR: [
+            {
+              userName: {
+                contains: term,
+              },
+            },
+            {
+              firstName: {
+                contains: term,
+              },
+            },
+            {
+              lastName: {
+                contains: term,
+              },
+            },
+          ],
+        },
+      });
+    },
+  },
+};
