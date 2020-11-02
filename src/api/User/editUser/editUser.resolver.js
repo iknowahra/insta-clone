a46787a57810f6f66e2c0c9dd3872738364f6565
iknowahra <iknowahrajoh@gmail.com>
2020-11-02 19:52:43 +0700

    edit user profile

---
+++ src/api/User/editUser/editUser.resolver.js
@@ -0,0 +1,19 @@
+export default {
+  Mutation: {
+    editUser: async (_, args, { request, prisma, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { userName, email, firstName, lastName, bio } = args;
+      const { user } = request;
+      return await prisma.user.update({
+        where: { id: user.id },
+        data: {
+          userName,
+          email,
+          firstName,
+          lastName,
+          bio,
+        },
+      });
+    },
+  },
+};
